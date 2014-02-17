7350d69ab96f242059c0dd14d692c8572fba24ba
thesamovar <dan.goodman@ens.fr>
2014-02-17 16:54:07 -0500

    Improved adex model explorer example to make it faster between runs

--- tools/model_explorer/examples/adex.py
+++ tools/model_explorer/examples/adex.py
@@ -8,36 +8,50 @@ integrate and fire model.
 from brian2 import *
 from model_explorer import *
 
+# Switch this on to run it a bit faster if you have gcc installed
+#brian_prefs.codegen.target = 'weave'
+
+G = None
+spikemon = None
+statemon = None
+cur_Nr = None
+
 def get_adex_data(model,
                   taum, tauw,
                   a, b,
                   DeltaT, EL, VT, Vr,
                   duration, silent, Imin, Imax, N, repeats,
                   ):
+    global G, spikemon, statemon, cur_Nr, cur_eqs
+    
     Vcut = VT + 5 * DeltaT
     Nr = N*repeats
     
     if DeltaT>0:
-        eqs = '''
-        dvm/dt = ((EL-vm)+DeltaT*exp((vm-VT)/DeltaT)+I-w)/taum : volt
-        dw/dt = (a*(vm-EL)-w)/tauw : volt
-        I : volt
-        '''
+        DeltaT_min = DeltaT
     else:
-        eqs = '''
-        dvm/dt = ((EL-vm)+I-w)/taum : volt
-        dw/dt = (a*(vm-EL)-w)/tauw : volt
-        I : volt
-        '''
+        DeltaT_min = 1*volt
+        
+    eqs = '''
+    dvm/dt = ((EL-vm)+DeltaT*exp((vm-VT)/DeltaT_min)+I-w)/taum : volt
+    dw/dt = (a*(vm-EL)-w)/tauw : volt
+    I : volt
+    '''
+        
+    if cur_Nr!=Nr:
+        G = NeuronGroup(Nr, eqs, threshold='vm>Vcut', reset='vm = Vr; w += b')
+        model.update() # maintain responsiveness of gui
+        spikemon = SpikeMonitor(G)
+        model.update() # maintain responsiveness of gui
+        statemon = StateMonitor(G, variables=['vm', 'w'], record=[0, Nr/3, 2*Nr/3, Nr-1])
+        model.update() # maintain responsiveness of gui
+        cur_Nr = Nr
 
-    G = NeuronGroup(Nr, eqs, threshold='vm>Vcut', reset='vm = Vr; w += b')
-    model.update() # maintain responsiveness of gui
+    spikemon.resize(0)
+    statemon.resize(0)
     G.vm = EL
     G.I = repeat(linspace(Imin, Imax, N), repeats)
-    spikemon = SpikeMonitor(G)
-    model.update() # maintain responsiveness of gui
-    statemon = StateMonitor(G, variables=['vm', 'w'], record=[0, Nr/3, 2*Nr/3, Nr-1])
-    model.update() # maintain responsiveness of gui
+    G.w = 0
     @network_operation
     def up():
         model.update(defaultclock.t/duration)
@@ -62,7 +76,7 @@ class AdExModel(ExplorableModel):
         Parameter('EL', -70*mV, -100*mV, 0*mV, 5*mV, unit=mV),
         Parameter('VT', -50*mV, -100*mV, 0*mV, 5*mV, unit=mV),
         Parameter('Vr', -70*mV, -100*mV, 0*mV, 5*mV, unit=mV),
-        Parameter('DeltaT', 2*mV, 0*mV, 10*mV, 0.5*mV, unit=mV),
+        Parameter('DeltaT', 0*mV, 0*mV, 10*mV, 0.5*mV, unit=mV),
 
         Parameter('duration', 500*ms, 0*ms, 10*second, 100*ms, unit=ms),
         Parameter('silent', 100*ms, 0*ms, 10*second, 100*ms, unit=ms),
@@ -106,6 +120,6 @@ class AdExModel(ExplorableModel):
         ax.set_xlabel('Time (ms)')
         ax.set_ylabel('w (mV)')
 
-
 if __name__=='__main__':
-    AdExModel().launch_gui()
+    model = AdExModel()
+    model.launch_gui()
